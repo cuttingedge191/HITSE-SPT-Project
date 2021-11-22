c5225fa896fec0121dc90c9d0fd1554d03d24828
GavinBox <1161906117@qq.com>
2021-11-22 13:17:58 +0800

    修改页面

--- HIT_SE_SPT/src/main/java/com/example/hit_se_spt/controller/HelloController.java
+++ HIT_SE_SPT/src/main/java/com/example/hit_se_spt/controller/HelloController.java
@@ -26,27 +26,7 @@ public class HelloController {
     public String login(String email, String password, Model model, HttpSession session, HttpServletRequest request, HttpServletResponse response){
         System.out.println(email + " " + password);
         return "login";
-//        Subject subject = SecurityUtils.getSubject();
-//        UsernamePasswordToken token=new UsernamePasswordToken(email,password);
-//        try {
-//            session.setAttribute("username",email);
-//            subject.login(token);//执行登录
-//            // 保存cookie，实现自动登录
-//            Cookie cookie_username = new Cookie("username", email);
-//            // 设置cookie的持久化时间，1天
-//            cookie_username.setMaxAge( 24 * 60 * 60);
-//            // 设置为当前项目下都携带这个cookie
-//            cookie_username.setPath(request.getContextPath());
-//            // 向客户端发送cookie
-//            response.addCookie(cookie_username);
-//            return "succeed";
-//        }catch (UnknownAccountException e){
-//            model.addAttribute("msg","用户名错误");
-//            return  "login";
-//        }catch (IncorrectCredentialsException e){
-//            model.addAttribute("msg","密码错误");
-//            return "login";
-//        }
+
     }
 
     @RequestMapping("logup")
