2c71da2035d0e5a79a3e3874a9811655d6f3dd90
GavinBox <1161906117@qq.com>
2021-11-22 22:22:19 +0800

    inventory mapper 完成

---
+++ spt/src/main/java/com/hit/spt/pojo/GoodsInfo.java
@@ -0,0 +1,11 @@
+package com.hit.spt.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GoodsInfo {
+}
