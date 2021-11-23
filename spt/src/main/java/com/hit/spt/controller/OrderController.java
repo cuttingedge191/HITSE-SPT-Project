b9a160abb6644ecca7da4280d9d71f78a96e8df1
GavinBox <1161906117@qq.com>
2021-11-23 17:17:34 +0800

    添加订单跳转，o_id传递

--- spt/src/main/java/com/hit/spt/controller/OrderController.java
+++ spt/src/main/java/com/hit/spt/controller/OrderController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
@@ -14,6 +15,13 @@ public class OrderController {
 
     @RequestMapping("addOneOrder")
     public String addOneOrder(Model model) {
+        Integer OrderId = orderItemService.genOrderId();
+        model.addAttribute("o_id", OrderId);
+        return "addOrder";
+    }
+
+    @RequestMapping("addOneOrderItem")
+    public String orderItemInfo(@RequestParam("o_id") Integer o_id, @RequestParam("name") String name, @RequestParam("quantity") Integer quantity, Model model) {
         Integer OrderId = orderItemService.genOrderId();
         model.addAttribute("o_id", OrderId);
         return "addOrder";
