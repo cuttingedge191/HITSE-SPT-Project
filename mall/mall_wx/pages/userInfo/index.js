0b89942dae4301d3c3c328aa4c32b73dba1e6431
txy <2679150788@qq.com>
2021-12-15 22:41:37 +0800

    ./mall

--- mall/mall_wx/pages/userInfo/index.js
+++ mall/mall_wx/pages/userInfo/index.js
@@ -1,20 +1,69 @@
-// pages/userInfo/index.js
+// pages/logup/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    gender: '1',
+    password: '',
+    ackpassword:'',
+    username: '测试',
+    phone: '1383838383838',
+    addr: 'false',
+    checkpw:'',
+    src:"https://img.yzcdn.cn/vant/cat.jpeg"
   },
 
+  //选择性别
+  onChange(event) {
+    this.setData({
+      radio: event.detail,
+    });
+  },
+ //注册
+ changeInfo: function() {
+     wx.switchTab({
+      url: '/pages/user/index'
+    })
+  },
+   // 上传头像
+   changeImage: function () {
+    var that = this;
+    wx.chooseImage({
+        count: 1, // 默认9
+        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+        success: function (res) {
+            // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片，只有一张图片获取下标为0
+            var tempFilePaths = res.tempFilePaths[0];
+            that.setData({
+                src: tempFilePaths,
+                actionSheetHidden: !that.data.actionSheetHidden
+            })
+            util.uploadFile('/itdragon/uploadImage', tempFilePaths, 'imgFile' ,{}, function (res) {
+                console.log(res);
+                if (null != res) {
+                    that.setData({
+                        userImg: res
+                    })
+                } else {
+                    // 显示消息提示框
+                    wx.showToast({
+                        title: '上传失败',
+                        icon: 'error',
+                        duration: 2000
+                    })
+                }
+            });
+        }
+    })    
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.navigateBack({
-      delta: 1
-  })
+        
   },
 
   /**
